8f645b3478581bf6287b1373860675eb8844f8ee
TheArchons <progamer9138@gmail.com>
2023-10-24 14:46:46 -0700

    add output file

--- autopuller.sh
+++ autopuller.sh
@@ -8,7 +8,7 @@ while; do
     if [[ $result == *"Your branch is behind"* ]]; then
         git stash
         git pull
-        echo "Pulled successfully on $(date)"
+        echo "Pulled successfully on $(date)" >> /home/archons/The-SQL-to-Water-H2O2.github.io/autopuller.log
     fi
     sleep 20
 done
