8b4376a734e30fd21c19c76986b7edf0f8d56ae6
TheArchons <progamer9138@gmail.com>
2023-10-24 14:20:38 -0700

    get autopuller to repeat every 20 seconds

--- autopuller.sh
+++ autopuller.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
-result=$(git status -uno)
+while; do
+    sleep 20
+    result=$(git status -uno)
 
-if [[ $result == *"Your branch is behind"* ]]; then
-    git stash
-    git pull
-fi
+    if [[ $result == *"Your branch is behind"* ]]; then
+        git stash
+        git pull
+        echo "Pulled successfully on $(date)"
+    fi
+done
